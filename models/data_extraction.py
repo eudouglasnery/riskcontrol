f4e1a7f9e03275593bb3dcb873dc0ce5321e41ee
DouglasNeryBr <dnery101@gmail.com>
2025-07-23 15:35:30 -0300

    feat: data extraction of tickers

---
+++ models/data_extraction.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import os
+from datetime import datetime, timedelta
+
+
+def extract_data(tickers: list, file_name="tickers_data.csv"):
+    project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    data_path = os.path.join(project_path, "", file_name)
+
+    end = datetime.today()
+    start = end - timedelta(days=180)  # last 6 months
+
+    tickers_data = yf.download(tickers, start=start, end=end)
+    tickers_data.dropna(inplace=True)
+    tickers_data.to_csv(data_path)
+
+    return tickers_data
