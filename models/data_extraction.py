4a6781e9a0df12915471f4b8820a6c91bba6b583
DouglasNeryBr <dnery101@gmail.com>
2025-07-25 16:12:55 -0300

    fix: fixing data extraction to save only column "Close" of the values

--- models/data_extraction.py
+++ models/data_extraction.py
@@ -10,7 +10,7 @@ def extract_data(tickers: list, file_name="tickers_data.csv"):
     end = datetime.today()
     start = end - timedelta(days=180)  # last 6 months
 
-    tickers_data = yf.download(tickers, start=start, end=end)
+    tickers_data = yf.download(tickers, start=start, end=end)["Close"]
     tickers_data.dropna(inplace=True)
     tickers_data.to_csv(data_path)
 
